3c5ca34c860d0f8782474d44367b698b6f357017
FyroozKhan <fyrooz.16@gmail.com>
2026-04-11 20:14:05 -0400

    Update utils.py

--- Code/src/utils.py
+++ Code/src/utils.py
@@ -4,3 +4,22 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import numpy as np
+
+
+def add_class_name_prefix(df: pd.DataFrame, col_name: str) -> pd.DataFrame:
+    """
+    Adds a class name prefix to the given filename column in a DataFrame.
+
+    Args:
+        df (pd.DataFrame): Input pandas DataFrame.
+        col_name (str): Name of the column containing filenames.
+
+    Returns:
+        pd.DataFrame: DataFrame with modified column.
+    """
+    df_copy = df.copy()
+    df_copy[col_name] = df_copy[col_name].apply(
+        lambda x: x[re.search("_", x).start() + 1: re.search("_", x).start() + 2]
+                  + "/" + x
+    )
+    return df_copy
