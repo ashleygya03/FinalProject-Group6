614dc86c4f8784462867a826fc4251b75d9e2814
Fyrooz Anika Khan <fyrooz.16@gmail.com>
2026-04-26 10:28:01 -0400

    Modify dataset path and update class count
    Updated the base path for the dataset and changed the number of classes from 29 to 26.

--- Code/src/config_mobilenetv2.py
+++ Code/src/config_mobilenetv2.py
@@ -1,7 +1,8 @@
 """Configuration constants for the ASL classification project."""
 import os
 
-BASE_PATH = "../Data/asl_alphabet_train/asl_alphabet_train/"
+# Merged dataset path (use this AFTER running merge_datasets.py):
+BASE_PATH = "../Data_3/merged-asl"
 WORKING_PATH = "./working/"
 TRAIN_PATH = os.path.join(WORKING_PATH, 'train')
 VAL_PATH = os.path.join(WORKING_PATH, 'val')
@@ -10,12 +11,11 @@ TEST_PATH = os.path.join(WORKING_PATH, 'test')
 IMAGE_SIZE = 224
 IMG_CHANNEL = 3
 BATCH_SIZE = 32
-N_CLASSES = 29
+N_CLASSES = 26
 SEED = 1333
 
 CATEGORIES = {
     0: "A", 1: "B", 2: "C", 3: "D", 4: "E", 5: "F", 6: "G", 7: "H", 8: "I", 9: "J",
     10: "K", 11: "L", 12: "M", 13: "N", 14: "O", 15: "P", 16: "Q", 17: "R", 18: "S", 19: "T",
-    20: "U", 21: "V", 22: "W", 23: "X", 24: "Y", 25: "Z",
-    26: "del", 27: "nothing", 28: "space"
+    20: "U", 21: "V", 22: "W", 23: "X", 24: "Y", 25: "Z"
 }
